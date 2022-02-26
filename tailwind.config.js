c1397b069f95616d0f27534a8767bf70ec742c81
Richard <richard.wigley@gmail.com>
2022-02-26 12:39:46 +0000

    CSS content folders added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,10 @@ const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
   darkMode: 'class',
   content: [
+    './app/components/**/*',
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
+    './app/assets/images/**/*.svg',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
