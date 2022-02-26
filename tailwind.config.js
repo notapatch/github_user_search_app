db313b48b865466d73974733d6b2ccd10796d23e
Richard <richard.wigley@gmail.com>
2022-02-26 12:26:41 +0000

    CSS custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,37 @@ module.exports = {
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        skin: {
+          fill: {
+            accent: 'var(--fill--accent)',
+            base:   'var(--fill--base)',
+            'primary-button': 'var(--accent)',
+            'primary-button-hover': 'var(--accent--hover)',
+          },
+          helper: {
+            danger: 'var(--helper--danger)',
+          }
+        }
+      },
+      textColor: {
+        skin: {
+          accent: 'var(--accent)',
+          helper: {
+            danger: 'var(--helper--danger)',
+          },
+          'primary-button': 'var(--text--primary--button)',
+          100: 'var(--text--100)',
+          '100--hover': 'var(--text--100--hover)',
+          200: 'var(--text--200)',
+          300: 'var(--text--300)',
+          400: 'var(--text--400)',
+        },
+      }
+    }
+  },
   plugins: [
     require('@tailwindcss/forms'),
   ]
