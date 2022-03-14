6542e681dccbce1db37cd644d1e54d1150943ea2
Richard <richard.wigley@gmail.com>
2022-03-14 19:31:35 +0000

    Company link has @ removed when prefixes company name

--- app/helpers/dev_helper.rb
+++ app/helpers/dev_helper.rb
@@ -2,4 +2,8 @@ module DevHelper
   def display_name(dev)
     dev.name || dev.login
   end
+
+  def clear_mention(mentionable_text)
+    mentionable_text.delete_prefix("@")
+  end
 end
--- spec/helpers/dev_helper_spec.rb
+++ spec/helpers/dev_helper_spec.rb
@@ -14,4 +14,24 @@ RSpec.describe DevHelper, type: :helper do
       expect(display_name(dev)).to eq "oct"
     end
   end
+
+  describe "clear_mention" do
+    it "removes mention when present" do
+      dev = Dev.new({ "company" => "@company" })
+
+      expect(clear_mention(dev.company)).to eq "company"
+    end
+
+    it "unchanged when mention after prefix" do
+      dev = Dev.new({ "company" => "c@mpany" })
+
+      expect(clear_mention(dev.company)).to eq "c@mpany"
+    end
+
+    it "unchanged when mention not present" do
+      dev = Dev.new({ "company" => "company" })
+
+      expect(clear_mention(dev.company)).to eq "company"
+    end
+  end
 end
